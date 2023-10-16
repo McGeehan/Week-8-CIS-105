e6743d451b639390ce11fab34aa6a6ed36619b61
MkGee <matthewrmcgeehan2@gmail.com>
2023-10-16 11:43:10 -0400

    Update avg_score_fixed.py

--- avg_score_fixed.py
+++ avg_score_fixed.py
@@ -2,8 +2,8 @@ def get_average(scores):
     total = 0
     for score in scores:
         total += score
-    average = total / len(scores)  # Fixed Bug 2
+    average = total / len(scores)  # Fixed Bug 1
     return average
 
 scores = [90, 80, 70, 60, 50]
-print("The average score is: ", get_average(scores))  # Fixed Bug 3
+print("The average score is: ", get_average(scores))  # Fixed Bug 2
