7c373b95ea90b2e51e671ff8980229f77a076824
MkGee <matthewrmcgeehan2@gmail.com>
2023-10-18 11:21:57 -0400

    Add files via upload

---
+++ hogwarts.py
@@ -0,0 +1,15 @@
+students = ["Hermione", "Ron", "Harry"]
+
+#manual method
+print(students[0])
+print(students[1])
+print(students[2])
+
+#iterative method
+for s in students:
+    print(s)
+
+#python initializes variables so it can just exist in the loop! s can be anything :)
+
+
+
