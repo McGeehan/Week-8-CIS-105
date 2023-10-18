c2709fcc11e27e492eb2aff97e8b0167041bcac3
MkGee <matthewrmcgeehan2@gmail.com>
2023-10-18 11:08:31 -0400

    Class Opener

---
+++ spaghetti_code.py
@@ -0,0 +1,11 @@
+#Ask user for their name
+name = input("What's your name?")
+
+#remove whitespace from their name
+name = name.strip()
+
+#capitalize user's name
+name = name.title()
+
+print(f"hello, {name}")
+
